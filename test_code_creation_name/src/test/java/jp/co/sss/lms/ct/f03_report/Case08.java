250af27cb8bd712a260088190621f202e286ca0d
印南茉里乃 <@192.168.111.106>
2025-09-25 17:34:38 +0900

    add:Case08.java 入力したレポートが反映するテストコード追加
    WebElementを利用してレポートを登録し、ユーザー詳細画面で反映されているか確認するテストを作成した。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,49 +41,128 @@ public class Case08 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		String pageTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+
+		//カレントウインドウのサイズを設定する
+		//		Dimension targetSize = new Dimension(900, 800);
+		//		webDriver.manage().window().setSize(targetSize);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ユーザー,パスワードを入力し、Enterキーを押下する
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		password.clear();
+
+		loginId.sendKeys("StudentAA01");
+		password.sendKeys("StudentAA02");
+		login.sendKeys(Keys.ENTER);
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("コース詳細 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		List<WebElement> detail = webDriver.findElements(By.className("btn-default"));
+		WebElement detailNumber = detail.get(2);
+
+		detailNumber.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("セクション詳細 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		List<WebElement> submitted = webDriver.findElements(By.className("btn-default"));
+		WebElement submittedNumber = submitted.get(3);
+
+		scrollTo("document.body.scrollHeight");
+
+		submittedNumber.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("レポート登録 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		final WebElement report = webDriver.findElement(By.id("content_1"));
+		final WebElement submission = webDriver.findElement(By.className("btn-primary"));
+
+		scrollTo("document.body.scrollHeight");
+
+		report.clear();
+
+		report.sendKeys("case08Test");
+		submission.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("セクション詳細 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
-		// TODO ここに追加
+		final WebElement userDetail = webDriver.findElement(By.cssSelector("a[href=\"/lms/user/detail\"]"));
+
+		userDetail.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("ユーザー詳細", pageTitle);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
-		// TODO ここに追加
+		List<WebElement> detail = webDriver.findElements(By.className("btn-default"));
+		WebElement detailButton = detail.get(6);
+
+		scrollTo("document.body.scrollHeight");
+
+		detailButton.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("レポート詳細 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 }

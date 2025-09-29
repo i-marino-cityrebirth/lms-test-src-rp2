799d5ba2be62459e855b5486487dcfe0c9bf1d76
印南茉里乃 <@192.168.111.106>
2025-09-29 14:07:58 +0900

    add:Case017.java パスワード変更からコース詳細画面への遷移テストコード追加
    WebElementを利用してタイトルの値を取得し、期
    待値通りかどうか確認するテストを作成いた。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,28 +41,90 @@ public class Case17 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		String pageTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ユーザー,パスワードを入力し、Enterキーを押下する
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+
+		loginId.clear();
+		password.clear();
+
+		loginId.sendKeys("StudentAA03");
+		password.sendKeys("StudentAA03");
+		login.sendKeys(Keys.ENTER);
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("セキュリティ規約 | LMS", pageTitle);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+		final WebElement consent = webDriver.findElement(By.className("btn-primary"));
+		final WebElement securityCheck = webDriver.findElement(By.name("securityFlg"));
+
+		scrollTo("document.body.scrollHeight");
+
+		securityCheck.click();
+
+		consent.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("パスワード変更 | LMS", pageTitle);
+
+		scrollTo("document.body.scrollHeight");
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
-	void test04() {
-		// TODO ここに追加
+	void test04() throws InterruptedException {
+		final WebElement password = webDriver.findElement(By.id("currentPassword"));
+		final WebElement newPassword = webDriver.findElement(By.id("password"));
+		final WebElement passwordConfirm = webDriver.findElement(By.id("passwordConfirm"));
+		List<WebElement> change = webDriver.findElements(By.className("btn-primary"));
+		WebElement changeButton = change.get(1);
+
+		password.sendKeys("StudentAA03");
+		newPassword.sendKeys("StudentAA04");
+		passwordConfirm.sendKeys("StudentAA04");
+
+		scrollTo("document.body.scrollHeight");
+
+		changeButton.click();
+
+		Thread.sleep(2000);
+
+		final WebElement modalChange = webDriver.findElement(By.id("upd-btn"));
+
+		modalChange.click();
+
+		String pageTitle = webDriver.getTitle();
+		assertEquals("コース詳細 | LMS", pageTitle);
+
+		scrollTo("document.body.scrollHeight");
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
